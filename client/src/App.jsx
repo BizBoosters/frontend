6b712e80c91192592cf401d42019661c420add48
Parteek5957K <parteek.mittal19@gmail.com>
2024-08-10 12:08:59 +0530

    Message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
-import Chatbot from './Components/Chatbot'
-import Footer from './Components/Footer'
-import Home from './Pages/Home'
-
-const App = () => {
+import { NavbarSimple } from './Components/Navbar'
+function App() {
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/blogs">Blogs</Link>
-        <Link to="/community">Community</Link>
-        <Link to="/raisefunds">Raise Funds</Link>
-      </nav>
+      <NavbarSimple/>
     </>
   )
 }
