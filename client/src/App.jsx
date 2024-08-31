58cc94e5ddcf0ccd643373c04a272c6af7e25602
Parteek5957K <parteek.mittal19@gmail.com>
2024-08-31 23:02:13 +0530

    message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,20 @@
-
 import React from 'react'
 import Home from './Pages/Home'
 import {Route, Routes} from "react-router-dom";
 import Blogs from './Pages/Blogs';
 import Chatbot from './Components/Chatbot'; 
+import Chatbot1 from './Components/Chatbot1';
 
 // import { ProductTwo } from './Components/Cards1'
 // import { Card } from '@material-tailwind/react'
 function App() {
   return (
     <>
-
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/blogs' element={<Blogs/>}/>
       <Route path='/Chatbot' element={<Chatbot/>}/>
+      <Route path='/Chatbot1' element={<Chatbot1/>}/>
     </Routes>
-
     </>
   )
 }
